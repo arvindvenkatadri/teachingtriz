035e3db44963c620afea59f73e11b25ab83d85b7
Arvind Venkatadri <arvind.venkatadri@gmail.com>
2024-07-04 11:21:34 +0530

    Summary and flow

--- content/courses/Analytics/Descriptive/Modules/10-FavStats/index.R
+++ content/courses/Analytics/Descriptive/Modules/10-FavStats/index.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(mosaic)
 library(skimr)
+library(knitr)
 library(kableExtra)
 
 
@@ -12,6 +13,32 @@ library(explore) # fake data generation
 library(grateful)
 # library(conflicted)
 # conflicts_prefer(dplyr::filter, dplyr::last, dplyr::glimpse, base::max)
+
+literacy <- readxl::read_xlsx("../../../../../materials/Data/US_literacy_SETables.xlsx",sheet = "S1",skip = 3) %>% 
+  select(-c(2,3),-contains("S.E.")) %>% 
+  rename("Numbers" = `...1`,
+         "BelowLevel1" = `Estimate...4`,
+         "Level1" = `Estimate...6`,
+         "Level2" = `Estimate...8`,
+         "Level3" = `Estimate...10`,
+         "Level4/5" = `Estimate...12`) %>% 
+  filter(str_detect(pattern = "Number",Numbers))
+
+literacy %>% 
+  kbl(caption = "US Population: Reading and Numeracy Levels", digits = 2,
+      align = "c",centering = T,
+      col.names = c("Year", "Below Level #1", "Level #1", "Level #2", "Level #3", "Levels #4 and #5")) %>% 
+  kable_paper(full_width = F, html_font = "Noto") %>% 
+  kable_styling(bootstrap_options = c("striped", "hover", "condensed"), position = "float_right") %>% 
+  column_spec(2:4, bold = T) %>%
+  row_spec(1:2, bold = T, color = "white", background = "#D7261E") %>% 
+    footnote(general = "SOURCE: U.S. Department of Education, National Center for Education Statistics, Program for the International Assessment of Adult Competencies (PIAAC), U.S. PIAAC 2017, U.S. PIAAC 2012/2014.")
+
+read_csv("../../../../../materials/Data/pronouns.csv") %>% 
+  #filter(No == "1") %>% 
+  kbl() %>%
+  kable_paper(c("striped","hover","responsive"), full_width = T)
+  
 
 mpg %>% 
   head(10) %>%
@@ -40,6 +67,15 @@ inspect(mpg)
 mpg_describe <- inspect(mpg)
 mpg_describe$categorical
 mpg_describe$quantitative
+
+
+mpg_modified <- mpg %>% 
+  dplyr::mutate(cyl = as_factor(cyl),
+                fl = as_factor(fl),
+                drv = as_factor(drv),
+                class = as_factor(class),
+                trans = as_factor(trans))
+glimpse(mpg_modified)
 
 
 # From Vincent Arel-Bundock's dataset website
@@ -71,14 +107,14 @@ skim(docVisits) %>% kbl()
 
 inspect(docVisits)
 
-diamonds %>% dplyr::count(cut)
-diamonds %>% mosaic::count(color) # does the same thing! Counts!
-diamonds %>% count(clarity)
+mpg_modified %>% dplyr::count(cyl)
+mpg_modified %>% mosaic::count(drv) # does the same thing! Counts!
+mpg_modified %>% count(fl)
 
 ### All combinations of cut, color, clarity
 ### Overwhelming??
-diamonds %>% 
-  count(across(where(is.ordered)))
+mpg_modified %>% 
+  count(across(where(is.factor)))
 
 
 ## Counting by the obvious factor variables
@@ -100,21 +136,21 @@ docVisits %>% count(illness)
 docVisits %>% count(health)
 
 
-diamonds %>% 
-  group_by(clarity) %>% 
-  summarize(average_price = mean(price), count = n())
+mpg_modified %>% 
+  group_by(cyl) %>% 
+  summarize(average_hwy = mean(hwy), count = n())
 
-diamonds %>% 
-  group_by(clarity, color) %>% 
-  summarize(average_price = mean(price), count = n())
+mpg_modified %>% 
+  group_by(cyl, fl) %>% 
+  summarize(average_hwy = mean(hwy), count = n())
 
 # Perhaps the best method for us!
-diamonds %>% 
-  mosaic::favstats(price ~ clarity, data = .) # Don't use fav_stats with formula!!!
+mpg_modified %>% 
+  mosaic::favstats(hwy ~ cyl, data = .) # Don't use fav_stats with formula!!!
 
 # Be aware of the first column format here!
-diamonds %>% 
-  mosaic::favstats(price ~ clarity + cut, data = .) # Don't use fav_stats with formula!!!
+mpg_modified %>% 
+  mosaic::favstats(hwy ~ cyl + fl, data = .) # Don't use fav_stats with formula!!!
 
 
 docVisits %>%
